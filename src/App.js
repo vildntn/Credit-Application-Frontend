fcf3c850c5df49f512308e9b8677e0e59a831d01
vildntn <vildant35@gmail.com>
2022-02-26 02:11:32 +0300

    project initialized

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import Dashboard from "./layouts/Dashboard";
+import NavBar from "./layouts/Navbar/NavBar";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import AddCreditApplication from "./pages/CreditApplication/AddCreditApplication";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer position="bottom-right" />
+      <div>
+        <NavBar />
+      </div>
+      <div style={{ marginTop: "4em" }}>
+      <Dashboard/>
+      </div>
     </div>
   );
 }
